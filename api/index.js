52c8a38a0b64ec3747a8e4d0cd56deed2826781a
Banti Singh <testingvisitor896@gmail.com>
2025-06-12 14:40:11 +0530

    without db vercel

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const serverless = require('serverless-http');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const metadataRoutes = require('../src/routes/metadata.routes');
+//const metadataRoutes = require('../src/routes/metadata.routes');
 const { handleError } = require('../src/middlewares/ErrorHandler');
 
 const app = express();
@@ -10,7 +10,12 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
-app.use('/metadata', metadataRoutes);
+//app.use('/metadata', metadataRoutes);
+
+// Temporary test route without DB or routes
+app.get('/test', (req, res) => {
+  res.json({ message: 'API is working without DB connection!' });
+});
 
 app.use(handleError);
 
