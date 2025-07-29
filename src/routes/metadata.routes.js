094f2af7bdcba8b47999cbed0a45a17c67fc2d3f
Banti Singh <124371011+bantisingh20@users.noreply.github.com>
2025-07-29 15:51:43 +0530

    Update metadata.routes.js

--- src/routes/metadata.routes.js
+++ src/routes/metadata.routes.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const {  listTablesAndViews ,ListReport ,GetReportById ,CheckRelationAndListOfColumn ,PreviewReport ,SaveReportConfiguration} = require('../controller/metadata.controller');
+const { PaginatedReport } = require('../models/metadata.model');
 
 router.get('/', (req, res) => {
   res.json({ message: 'Metadata API working!' });
 });
-
-//list page
-router.get('/List-Report', ListReport); 
-router.get('/report/:id', GetReportById);
-
-// // //submit 
+  
 router.get('/tables',listTablesAndViews);
 router.post('/check-table-relations', CheckRelationAndListOfColumn);
-router.post('/report/preview', PreviewReport);
+router.post('/report/preview', PreviewReport); 
+router.get('/report/pagination', PaginatedReport);
 router.post('/report/save/:id', SaveReportConfiguration);
 
+router.get('/List-Report', ListReport); 
+router.get('/report/:id', GetReportById);
 module.exports = router;
